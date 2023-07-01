3579e75a4984dc562287c6ee70ece0ed014701ec
bebeto123 <bebeto123@mail.ru>
2023-07-01 23:02:05 +0300

    fix тестовых запросов из статики

--- dist/static/script.js
+++ dist/static/script.js
@@ -1,35 +1,25 @@
 'use strict';
-fetch('http://localhost:3030/posts', {
-    method: 'POST',
-})
+fetch('http://localhost:3030/json/posts')
     .then((response) => response.json())
     .then((posts) => {
         console.log({ posts });
     });
-fetch('http://localhost:3030/todos', {
-    method: 'POST',
-})
+fetch('http://localhost:3030/json/todos')
     .then((response) => response.json())
     .then((todos) => {
         console.log({ todos });
     });
-fetch('http://localhost:3030/todos?search=react', {
-    method: 'POST',
-})
+fetch('http://localhost:3030/todos?search=react')
     .then((response) => response.json())
     .then((queryTodos) => {
         console.log({ queryTodos });
     });
-fetch('http://localhost:3030/posts?search=puppa', {
-    method: 'POST',
-})
+fetch('http://localhost:3030/posts?search=puppa')
     .then((response) => response.json())
     .then((queryTodos) => {
         console.log({ queryTodos });
     });
-fetch('http://localhost:3030/todos2', {
-    method: 'POST',
-})
+fetch('http://localhost:3030/todos2')
     .then((response) => response.json())
     .then((todos2) => {
         console.log({ todos2 });
